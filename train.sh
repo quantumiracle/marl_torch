0548a3e1a75397e9885d32006985958dd9e6927a
zihan <zhding@mail.ustc.edu.cn>
2021-04-11 21:51:21 -0400

    results

--- train.sh
+++ train.sh
@@ -3,6 +3,6 @@ echo "Running DATE:" $(date +"%Y-%m-%d %H:%M")
 DATE=`date '+%Y%m%d_%H%M'`
 
 # nohup python train_pettingzoo.py --env 'pong_v1' --ram  --selfplay > log/$DATE$RAND.log &
-nohup python train_pettingzoo_mp_vecenv.py --env 'pong_v1' --ram --num-envs 3 --selfplay > log/$DATE$RAND.log &
-# nohup python train_pettingzoo_mp_vecenv.py --env 'pong_v1' --num-envs 3 --selfplay > log/$DATE$RAND.log &
+#nohup python train_pettingzoo_mp_vecenv.py --env 'pong_v1' --ram --num-envs 3 --selfplay > log/$DATE$RAND.log &
+nohup python train_pettingzoo_mp_vecenv.py --env 'pong_v1' --num-envs 3 --selfplay > log/$DATE$RAND.log &
 # nohup python main.py --env 'pong_v1' --train-freq 1000 --batch-size 256 --eta 1  > log/$DATE$RAND.log &
