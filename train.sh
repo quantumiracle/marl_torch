b559af0598bfcf444245f09ae6036a0121dd11c0
quantumiracle <zhding@mail.ustc.edu.cn>
2021-06-30 14:33:37 +0800

    test slimevolley

--- train.sh
+++ train.sh
@@ -3,11 +3,12 @@ echo "Running DATE:" $(date +"%Y-%m-%d %H:%M")
 DATE=`date '+%Y%m%d_%H%M'`
 echo "Save as: " $DATE
 
+nohup python train_pettingzoo.py --env slimevolley_v0 --selfplay > log/$DATE$RAND.log &
 # nohup python train_pettingzoo.py --env 'pong_v1' --ram  --selfplay > log/$DATE$RAND.log &
 # nohup python train_pettingzoo_mp_vecenv.py --env 'pong_v1' --ram --num-envs 3 --selfplay > log/$DATE$RAND.log &
 #nohup python train_pettingzoo_mp_vecenv.py --env 'pong_v1' --num-envs 3 --selfplay > log/$DATE$RAND.log &
 #nohup python main.py --env 'pong_v1' --train-freq 1000 --batch-size 256 --eta 1  > log/$DATE$RAND.log &
 # nohup python train_pettingzoo.py --env pong_v1 --ram --selfplay > log/$DATE$RAND.log &
 #nohup python exploit_selfplay.py --env boxing_v1 --ram > log/$DATE$RAND.log &
-nohup python exploit_selfplay.py --env pong_v1 --ram > log/$DATE$RAND.log &
+# nohup python exploit_selfplay.py --env pong_v1 --ram > log/$DATE$RAND.log &
 
